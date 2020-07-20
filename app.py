5a7611d2d9b8b2a7be2da190b423509bbc4c0f27
paulorss <61366907+paulorss@users.noreply.github.com>
2020-07-20 14:49:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
 # -*- coding: utf-8 -*-
-import unidecode
-from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
-from chatterbot.logic import LogicAdapter
-from chatterbot.response_selection import get_first_response
-from chatterbot.comparisons import levenshtein_distance
-from chatterbot.response_selection import *  # get_first_response
-from chatterbot.comparisons import *  # levenshtein_distance
+from chatterbot.response_selection import *
+from chatterbot.comparisons import *
 from chatterbot import *
 import csv
-import json
-import sys
-import os
 from flask import Flask, render_template, request
 from bs4 import BeautifulSoup
 import requests
@@ -21,16 +13,9 @@ from newspaper import Article
 from newspaper import news_pool
 from googlesearch import search
 import wikipedia
-from logging import *
-import logging
-import urllib3
-import sys
-from chatterbot.adapters import Adapter
-from chatterbot.storage import StorageAdapter
-from chatterbot.search import IndexedTextSearch
-from chatterbot.conversation import Statement
-
-
+#import urllib3
+#from chatterbot.adapters import Adapter
+#from chatterbot.storage import StorageAdapter
 
 #logging.basicConfig(filename="Log_Test_File.txt", level=logging.INFO, filemode='a')
 
@@ -40,92 +25,27 @@ searchbot = ChatBot(
     "Chatterbot",
     input_adapter="chatterbot.input.VariableInputTypeAdapter",
     output_adapter="chatterbot.output.OutputAdapter",
-    #storage_adapter="chatterbot.storage.MongoDatabaseAdapter",
+    # storage_adapter="chatterbot.storage.MongoDatabaseAdapter",
     # filters=[get_recent_repeated_responses],
     preprocessors=[
         'chatterbot.preprocessors.clean_whitespace',
         'chatterbot.preprocessors.convert_to_ascii'
-        #'custom_preprocessors.PreprocessorCustom1'
+        # 'custom_preprocessors.PreprocessorCustom1'
     ],
     logic_adapters=[
         {
             'import_path': "chatterbot.logic.BestMatch",
-            'statement_comparison_function': JaccardSimilarity,
-            'response_selection_method': get_first_response,
         },
-        {
-            'import_path': 'chatterbot.logic.SpecificResponseAdapter',
-            'input_text': 'teste especial',
-            'output_text': 'Ok, here is a link: http://chatterbot.rtfd.org'
-        },
-        {
-            'import_path': "chatterbot.logic.BestMatch",
-            'statement_comparison_function': "chatterbot.comparisons.Comparator",
-            'response_selection_method': get_first_response,
-
-        },
-        {
-            'import_path': 'chatterbot.logic.BestMatch',
-            'response_selection_method': get_first_response,
-            'statement_comparison_function': levenshtein_distance,
-            # 'statement_comparison_function': "ChatBot.comparisons.Comparator",
-            # 'statement_comparison_function': "chatterbot.comparisons.SynsetDistance",
-            #'statement_comparison_function': SentimentComparison,
-            # 'statement_comparison_function': "chatterbot.comparisons.JaccardSimilarity",
-            #'default_response': 'Desculpe, não compreendi a pergunta! Poderia informar o assunto desejado?',
-            #'maximum_similarity_threshold': 0.5
-        },
-        {
-            'import_path': 'chatterbot.logic.BestMatch',
-            'response_selection_method': get_first_response,
-            #'statement_comparison_function': "ChatBot.comparisons.levenshtein_distance",
-            # 'statement_comparison_function': "ChatBot.comparisons.Comparator",
-            'statement_comparison_function': SynsetDistance,
-            # 'statement_comparison_function': SentimentComparison,
-            # 'statement_comparison_function': "chatterbot.comparisons.JaccardSimilarity",
-            #'default_response': 'Desculpe, não compreendi a pergunta! Poderia informar o assunto desejado?',
-            #'maximum_similarity_threshold': 0.5
-        },
-        {
-            'import_path': 'chatterbot.logic.BestMatch',
-            'response_selection_method': get_most_frequent_response,
-            # 'statement_comparison_function': "ChatBot.comparisons.levenshtein_distance",
-            # 'statement_comparison_function': "ChatBot.comparisons.Comparator",
-            # 'statement_comparison_function': "chatterbot.comparisons.SynsetDistance",
-            'statement_comparison_function': SentimentComparison,
-            #'statement_comparison_function': JaccardSimilarity,
-            #'default_response': 'Desculpe, não compreendi a pergunta! Poderia informar o assunto desejado?',
-            #'maximum_similarity_threshold': 0.5,
-            # 'search_algorithm_name': jaccard_similarity,
-        },
-        'chatterbot.logic.BestMatch'
-        #JaccardSimilarity,
-        #'chatterbot.get_most_frequent_response',
-        #'chatterbot.logic.TimeLogicAdapter',
-        #'chatterbot.logic.MathematicalEvaluation'
-        #
-
     ],
-    storage_adapter="chatterbot.storage.MongoDatabaseAdapter",
-    #database='heroku_t1jjs2t8',
-    database_uri="mongodb://heroku_t1jjs2t8:hkvs6ok11go5kam96evri3p22a@ds041536.mlab.com:41536/heroku_t1jjs2t8?retryWrites=false&w=majority"
-    #database_uri="mongodb+srv://P15l4r4b2rt4:<password>@botheroku.j2haj.gcp.mongodb.net/<dbname>?retryWrites=false&w=majority"
-    #database_uri='sqlite:///database.db',
-    # statement_comparison_function=levenshtein_distance,
-    # response_selection_method=get_first_response
     read_only=True
 )
-
 
 trainer = ListTrainer(searchbot)
 
 conv = open('chats.csv', encoding='utf-8').readlines()
-#convex = open('export.json').readlines()
 
 trainer.train(conv)
-#trainer.train(convex)
-# trainer.train(convirpf)
-# trainer.train(convcpf)
+
 
 trainer.export_for_training('./export.json')
 
@@ -136,7 +56,6 @@ def index():
 
 
 @app.route("/get")
-# function for the bot response
 def get_bot_response():
     while True:
         userText = request.args.get('msg')
@@ -145,8 +64,12 @@ def get_bot_response():
         f = csv.writer(open('inputs.csv', 'a', encoding='utf-8'))
         f.writerow([msg])
         response = searchbot.get_response(userText)
-        if float(response.confidence) >= 0.6:
+        if float(response.confidence) >= 0.5:
             return str(searchbot.get_response(userText))
+        elif userText == str('NÃO'):
+            return str('Refaça a pergunta, por favor!')
+        elif userText == str("SIM"):
+            return str("Agradecemos o seu contato!")
         elif float(response.confidence) == 0.0:
             entrada = msg
             # print(entrada)
@@ -199,7 +122,7 @@ def get_bot_response():
 
             listag = []
             rec = 'site:receita.economia.gov.br intitle:' + msg + " -filetype:pdf -.pdf"
-            for urla in search(rec, tld='com.br', lang='pt-br', stop=4, pause=7):
+            for urla in search(rec, tld='com.br', lang='pt-br', stop=4, pause=10):
                 listag.append(urla)
 
             g = int(len(listag))
@@ -226,8 +149,8 @@ def get_bot_response():
             j = len(conj)
 
             reports2 = []
-            # news_pool.set(reports2)#, threads_per_source=2)
-            # news_pool.join()
+            news_pool.set(reports2)#, threads_per_source=2)
+            news_pool.join()
             for r in range(0, j):
 
                 try:
@@ -254,16 +177,15 @@ def get_bot_response():
                 content = page
                 return str(content)
             else:
-                resposta_final = (str(resposta_finalc).replace('\n', ' ').replace('[', ' ').replace(']', ' ').replace(',', ' ').replace("'", ' ').replace('{', ' ').replace("}", ' '))
+                resposta_final = (
+                    str(resposta_finalc).replace('\n', ' ').replace('[', ' ').replace(']', ' ').replace(',',
+                                                                                                        ' ').replace(
+                        "'", ' ').replace('{', ' ').replace("}", ' '))
 
                 f = csv.writer(open('chats.csv', 'a', encoding='utf-8'))
                 f.writerow([msg + '\n' + resposta_final])
-                return str(resposta_final + '\n' + 'Ficou satisfeito com a resposta?')
-
-
-
-
-            # return str(resposta_final)
+                return str(resposta_final + '\n' + 'Ficou satisfeito com a resposta? SIM ou NÃO?')
+        # return str(resposta_final)
 
 
 if __name__ == '__main__':
